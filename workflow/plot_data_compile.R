cf408867e56c09a0d62f37415fbfb30674c8d5d5
Adam M. Wilson <900623+adammwilson@users.noreply.github.com>
2023-10-06 13:43:14 +0200

    update plot aggregation

--- workflow/plot_data_compile.R
+++ workflow/plot_data_compile.R
@@ -65,7 +65,7 @@ sitesheet=read_sheet(sheet,"SiteData") %>%
 
 ### Quadrat data
 # Filter using grep to identify only plot and drop template sheets
-plot_sheets <- sheets[grepl("plot", sheets) & !grepl("Template_Plot|Template_plot|Example_plot", sheets) & 
+plot_sheets <- sheets[grepl("plot", sheets,ignore.case = T) & !grepl("Template_Plot|Template_plot|Example_plot", sheets) & 
                         !grepl("Swartberg_20_plot",sheets) & #swartberg 20 is empty
                         !grepl("Gardenroute_T275_plot",sheets)
                       ] 
@@ -73,9 +73,9 @@ plot_sheets <- sheets[grepl("plot", sheets) & !grepl("Template_Plot|Template_plo
 # Download data from the specified tabs as data frames
 data_downloaded <- lapply(plot_sheets, function(tab) {
 read_sheet(sheet,tab) %>% 
-    select(-SeasonallyApparent,
+    select(-SeasonallyApparent,  #drop field causing problems with weird entry in Rondevlei_147
            -NewSpecies,
-           -MeanCanopyDiameter_cm) %>%  #drop field causing problems with weird entry in Rondevlei_147
+           -MeanCanopyDiameter_cm) %>% 
     filter(!is.na(SiteCode_Plot_Quadrant))
     })
 
@@ -84,10 +84,9 @@ data <- data_downloaded %>%
   bind_rows() %>% 
   separate(SiteCode_Plot_Quadrant,into=c("SiteCode","Plot","Quadrant"),sep="_",remove = F) %>% 
   mutate("SiteCode_Plot"=paste(SiteCode,Plot,sep="_"),
-         Plot=as.numeric(sub("T","",Plot))) %>% 
-  left_join(sitesheet,by=c("SiteCode_Plot_Quadrant","SiteCode","SiteCode_Plot","Plot","Quadrant") ) 
+         Plot=as.numeric(sub("T","",Plot)))  
 
-### Transect data
+# add transect data?
 
 list(sites=sitesheet,quaddata=data)
 
@@ -95,15 +94,206 @@ list(sites=sitesheet,quaddata=data)
 
 
 # Use map to extract and combine specific elements from the inner lists
-allsites <- map(alldata, function(x) x["sites"][[1]] %>%  
-                  select(-PostFireAge_years)) %>%   # drop problem column due to varing inputs
-  bind_rows()
+sites <- map(alldata, function(x) x["sites"][[1]] %>%  
+                  select(-PostFireAge_years,
+                         -VegHeight_cm,
+                         -Date)) %>%   # drop problem column due to varying inputs     #NEED FIX
+  bind_rows()%>%
+  filter(!is.na(Plot))
 
 
-quaddata=map(alldata, function(x) x["quaddata"][[1]] %>%  
-               select(-PostFireAge_years)) %>%   # drop problem column due to varing inputs
-  bind_rows()
+quads=map(alldata, function(x) x["quaddata"][[1]]) %>%  
+#               select(-PostFireAge_years)) %>%   # drop problem column due to varing inputs         #NEED FIX
+  bind_rows()%>%
+  filter(!is.na(Plot))
 
+
+
+if(F){  # some EDA
+quads %>%
+  group_by(Plot,NameCheck,Genus_Species_combo) %>%
+  summarize(percent_cover=mean(PercentCoverAlive)) %>%
+  arrange(Plot,desc(percent_cover)) %>%
+  filter(is.na(NameCheck)) %>% View()
+
+  length(unique(quads$Plot))
+  length(unique(quads$Genus_Species_combo))
+
+  
+  
+  }
+
+
+### Update to new numbers
+## the original points had some duplicated numbers that were replaced
+# this section updates those numbers to the new system
+
+# get plots to update
+numchange = points %>%
+  mutate(old_plotnum=as.numeric(unlist(regmatches(old_plot, gregexpr("[0-9]+\\.?[0-9]*", old_plot))))) %>% 
+  filter(plotnum!=old_plotnum) 
+
+
+##########
+if(F){ # EDA on plot renumbering
+sites %>% 
+  select(Plot,SiteCode) %>% 
+  distinct() %>% 
+  arrange(Plot) %>% 
+  View()
+
+s1=unique(sites$Plot)[unique(sites$Plot)%in%numchange$old_plotnum] %>% sort()
+s1
+
+sites %>% filter(Plot%in%s1) %>% select(SiteCode, Plot) %>%  distinct() %>%  arrange(Plot)
+
+}
+
+
+## update specific plot numbers to the new scheme
+sites2 <- sites %>% 
+  mutate(old_plot=Plot) %>% 
+  select(-Plot) %>% 
+  mutate(Plot=case_when(  #update plot numbers
+    old_plot==20&grepl("swartberg",SiteCode,ignore.case=T) ~ 110,
+    old_plot==22&grepl("swartberg",SiteCode,ignore.case=T) ~ 12,
+    old_plot==23&grepl("swartberg",SiteCode,ignore.case=T) ~ 13,
+    old_plot==24&grepl("swartberg",SiteCode,ignore.case=T) ~ 14,
+  TRUE ~ old_plot
+  ),
+  Plot=paste0("T",sprintf("%03d", Plot)),
+  SiteCode_Plot = paste(SiteCode,Plot,sep="_"),
+  SiteCode_Plot_Quadrant = paste(SiteCode,Plot,Quadrant,sep="_"),
+  ) %>% 
+  select(Plot,SiteCode,SiteCode_Plot,SiteCode_Plot_Quadrant,Quadrant,everything()) %>% 
+  arrange(Plot,Quadrant)# %>% 
+#  select(old_plot,Plot,Plot2,SiteCode_Plot_Quadrant)
+  #select(SiteCode,Plot,SiteCode_Plot,Quadrant,SiteCode_Plot_Quadrant,geom) %>% 
+#  st_as_sf()
+
+
+## update quad plot numbers to the new scheme
+quads2 <- quads %>% 
+  mutate(old_plot=Plot) %>% 
+  select(-Plot) %>% 
+  mutate(Plot=case_when(  #update plot numbers
+    old_plot==20&grepl("swartberg",SiteCode,ignore.case=T) ~ 110,
+    old_plot==22&grepl("swartberg",SiteCode,ignore.case=T) ~ 12,
+    old_plot==23&grepl("swartberg",SiteCode,ignore.case=T) ~ 13,
+    old_plot==24&grepl("swartberg",SiteCode,ignore.case=T) ~ 14,
+    TRUE ~ old_plot
+  ),
+  Plot=paste0("T",sprintf("%03d", Plot)),
+  SiteCode_Plot = paste(SiteCode,Plot,sep="_"),
+  SiteCode_Plot_Quadrant = paste(SiteCode,Plot,Quadrant,sep="_"),
+  ) %>% 
+  select(Plot,SiteCode,SiteCode_Plot,SiteCode_Plot_Quadrant,Quadrant,everything()) %>% 
+  arrange(Plot,Quadrant)
+
+
+if(F){ # EDA
+  # confirm all plots match in site and quad data
+  complete_sites=unique(sites2$Plot) %>% sort()
+  complete_quadsites=unique(quads2$Plot) %>% sort()
+  complete_sites[!complete_sites%in%complete_quadsites]
+  complete_quadsites[!complete_quadsites%in%complete_sites]
+
+  checkq="T009"  
+  filter(points,plot==checkq)
+  filter(quads2,old_plot==9)#Plot==checkq)
+  
+  table(sites2$Plot%in%quads2$Plot)
+  table(quads2$Plot%in%sites2$Plot)
+}
+# quads2 %>% mutate(plotnum=as.numeric(sub("T","",Plot))) %>% filter(plotnum!=old_plot) %>% select(SiteCode_Plot_Quadrant,Plot,plotnum, old_plot)
+
+# date tag for filenames and the release
+tag=paste0("v",gsub("-","",lubridate::today()))
+
+
+# write site data at quad level
+f_quadrat_summary=file.path("output",paste0("bioscape_veg_quadrat_summary_",tag,".csv"))
+
+sites2 %>% 
+  write_csv(f_quadrat_summary)
+
+# site level
+f_plot_summary=file.path("output",paste0("bioscape_veg_plot_summary_",tag,".csv"))
+
+sites2 %>% 
+  group_by(Plot,SiteCode,SiteCode_Plot,GPS_PlotCentre,Observer) %>% 
+  summarize(PercentBareSoil=mean(PercentBareSoil,na.rm=T),
+            PercentBareRock=mean(PercentBareRock,na.rm=T),
+            PercentDeadVegetation=mean(PercentDeadVegetation,na.rm=T),
+            PercentLiveVegetation=mean(PercentLiveVegetation,na.rm=T),
+            SoilDepth_cm=mean(SoilDepth_cm),
+            Groundwater=first(Groundwater),
+            Access_Notes=first(Access_Notes),
+            sampled=1) %>%
+  arrange(Plot) %>% 
+  left_join(select(points,Plot=plot,nearest_reserve,geom)) %>% 
+  write_csv(f_plot_summary)
+
+
+## Species level
+f_quadrat_species=file.path("output",paste0("bioscape_veg_quadrat_species_",tag,".csv"))
+
+quads2 %>% 
+  arrange(Plot) %>% 
+  write_csv(f_quadrat_species)
+
+## Plot Dominant Summary -  mean cover, etc. for each plot
+f_plot_species=file.path("output",paste0("bioscape_veg_plot_species_",tag,".csv"))
+
+quads2 %>% 
+  group_by(Plot,SiteCode,SiteCode_Plot,AcceptedGenus,AcceptedSpecies,NameCheck,Genus_Species_combo) %>%
+  summarize(PercentCoverAlive=mean(PercentCoverAlive),
+            PercentCoverDead=mean(PercentCoverDead),
+            AbundanceAlive_count=sum(AbundanceAlive_count),
+            AbundanceDead_count=sum(AbundanceDead_count)
+            ) %>%
+  arrange(Plot,desc(PercentCoverAlive)) %>% 
+  write_csv(f_plot_species)
+
+# geopackage of plot data
+
+# kml of plot data
+
+
+
+# Data upload
+# upload summary data to release
+
+repo="BioSCape-io/BioSCape-terrestrial"
+
+# if release doesn't exist for this tag - create it
+if(!any(tag%in%pb_releases(repo)$tag_name))  pb_new_release(repo = repo,tag=tag)
+
+pb_upload(file = f_quadrat_summary,
+          repo="BioSCape-io/BioSCape-terrestrial",
+          tag=tag)
+
+pb_upload(file = f_plot_summary,
+          repo="BioSCape-io/BioSCape-terrestrial",
+          tag=tag)
+
+pb_upload(file = f_quadrat_species,
+          repo="BioSCape-io/BioSCape-terrestrial",
+          tag=tag)
+
+pb_upload(file = f_plot_species,
+          repo="BioSCape-io/BioSCape-terrestrial",
+          tag=tag)
+
+# 
+#  pb_upload(file = file.path("data",gpkgfile),
+#            repo="BioSCape-io/BioSCape-terrestrial",
+#            tag=tag)
+# 
+#  pb_upload(file = file.path("data",gpkgfile),
+#            repo="BioSCape-io/BioSCape-terrestrial",
+#            tag=tag)
+#  
 #OLD STUFF BELOW
 ##################################
 
@@ -123,62 +313,5 @@ allplots=st_read(file.path("data",gpkgfile)) %>%
   mutate(sampled_site=old_plot%in%sites$Plot, #identify which have site data
          sampled_cover=old_plot%in%data$Plot,
          sampled_homogeneous=old_plot%in%homogeneous_areas$plot) #identify which have cover data
-
-## summarize which plots have updated plot numbers:
-allplots %>% 
-  mutate(plot2=as.numeric(as.character(gsub("T","",plot)))) %>% 
-  filter(old_plot!=plot2)
-
-#table(allplots$sampled_cover,allplots$sampled_site)
-
-### Clean up new/old plot numbers
-
-# get plots to update
-numchange = points %>%
-  mutate(old_plotnum=as.numeric(unlist(regmatches(old_plot, gregexpr("[0-9]+\\.?[0-9]*", old_plot))))) %>% 
-  filter(plotnum!=old_plotnum) 
-
-sites= sitesheet %>% 
-  mutate(old_plot=Plot) %>% 
-  select(-Plot) %>% 
-  ##########
-# The next section renames plots that had to be adjusted due to "J" prefix and duplicate numbers
-mutate(plot=case_when(
-  old_plot==22&SiteCode=="swartberg" ~ 12,
-  old_plot==23&SiteCode=="swartberg" ~ 13,
-  old_plot==24&SiteCode=="swartberg" ~ 14,
-  TRUE ~ old_plot
-)) %>% 
-  left_join(points,by=c("plot"="plotnum","old_plot")) 
-
-
-
-
-
-# Data upload
-
-# upload summary data to release
-
-data_filename=paste0("bioscape_vegetation_data_",tag,".csv")
-
-tag=paste0("v",gsub("-","",lubridate::today()))
-repo="BioSCape-io/BioSCape-terrestrial"
-readr::write_csv(data,file.path("data",data_filename))
-
-# if release doesn't exist for this tag - create it
-if(!any(tag%in%pb_releases(repo)$tag_name))  pb_new_release(repo = repo,tag=tag)
-
-pb_upload(file = file.path("data",kmlfile),
-          repo="BioSCape-io/BioSCape-terrestrial",
-          tag=tag)
-
-pb_upload(file = file.path("data",gpkgfile),
-          repo="BioSCape-io/BioSCape-terrestrial",
-          tag=tag)
-
-
-pb_upload(file = file.path("data",data_filename),
-          repo="BioSCape-io/BioSCape-terrestrial",
-          tag=tag)
 
 
